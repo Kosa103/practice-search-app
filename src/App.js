4e67b50fcb1dff8033df88cc14798b0da5422e1b
Kosa103 <dotomiro2@gmail.com>
2020-11-10 07:01:01 +0100

    added search history, improved error boundary

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import { useState } from 'react';
+import * as React from 'react';
 import './App.css';
 import { ErrorBoundary } from 'react-error-boundary';
 import SearchBar from './modules/SearchBar';
 import ResultBox from './modules/ResultBox';
-import SearchHistory from './modules/SearchHistory';
+// import SearchHistory from './modules/SearchHistory';
 import fetchSearchData from './modules/searchLogic';
 
 
-function ErrorFallback({error}) {
+const SearchHistoryContext = React.createContext([]);
+
+
+function ErrorFallback({error, resetErrorBoundary}) {
   return (
-    <div>
-      <p>An error occured:</p>
-      <pre>{error.message}</pre>
+    <div className="error-box" onClick={resetErrorBoundary}>
+      <p className="error-pre-message">An error occured:</p>
+      <pre className="error-message">{error.message}</pre>
+      <p className="error-post-message">Click on this message to reset the display.</p>
     </div>
+  );
+}
+
+
+function SearchHistory() {
+  const context = React.useContext(SearchHistoryContext);
+  const history = context.history;
+  const load = context.load;
+
+
+  function renderSearchHistory() {
+    if (history.length && Array.isArray(history)) {
+      const list = history.map((person, index) => {
+        return (
+          <p key={`${person.id}-${index}`} 
+             onClick={() => load(person.id)}
+             className="search-history-element">
+               {person.name}
+          </p>
+        );
+      });
+      return list;
+    } else {
+      return (
+        <p>No search history</p>
+      );
+    }
+  }
+
+  return (
+      <div className="search-history-box">
+          <h3>SEARCH HISTORY:</h3>
+          {renderSearchHistory()}
+      </div>
   );
 }
 
@@ -21,7 +59,8 @@ function App() {
   const defaultText = 'To search for people type a name\n into the search bar and press "SEARCH"';
   const loadingText = "LOADING";
 
-  const [searchResult, setSearchResult] = useState(defaultText);
+  const [searchResult, setSearchResult] = React.useState(defaultText);
+  const [searchHistory, setSearchHistory] = React.useState([]);
 
 
   function getRandomInt(min, max) {
@@ -38,18 +77,46 @@ function App() {
 
 
   async function fetchData() {
+    const newSearchHistory = searchHistory;
+
     const searchData = await fetchSearchData();
+
+    if (searchData) {
+      if (!!searchData.length && Array.isArray(searchData)) {
+        newSearchHistory.unshift(searchData[0]);
+        setSearchHistory(newSearchHistory);
+      }
+    }
 
     setSearchResult(searchData || defaultText);
   }
 
+
+  function loadPersonFromCache(id) {
+    let cachedPerson = null;
+
+    for (const person of searchHistory) {
+      if (person.id === id) {
+        cachedPerson = person;
+        break;
+      }
+    }
+
+    setSearchResult([cachedPerson]);
+  }
+
+
   return (
     <div className="main-app-box">
       <SearchBar fetchCommand={() => startFetching} />
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <ErrorBoundary 
+        FallbackComponent={ErrorFallback}
+        onReset={() => setSearchResult(defaultText)}>
         <ResultBox searchResult={searchResult} />
       </ErrorBoundary>
-      <SearchHistory />
+      <SearchHistoryContext.Provider value={{history: searchHistory, load: loadPersonFromCache}}>
+        <SearchHistory />
+      </SearchHistoryContext.Provider>
     </div>
   );
 }
--- src/modules/SearchHistory.js
+++ src/modules/SearchHistory.js
@@ -1,18 +1,29 @@
+// Context didn't work through modules. Working on that...
+// Added contents to App.js
+
+/* import * as React from 'react';
+import SearchHistoryContext from '../App'
 import "./SearchHistory.css";
 
 
-function SearchHistory(props) {
+function SearchHistory() {
+    const history = React.useContext(SearchHistoryContext);
+
+
     function renderSearchHistory() {
         return (
             <p>Placeholder for search history</p>
         );
     }
 
+    console.log("SEARCH HISTORY:");
+    console.log(history);
     return (
         <div className="search-history-box">
-            {renderSearchHistory}
+            {renderSearchHistory()}
         </div>
     );
 }
 
 export default SearchHistory;
+ */
